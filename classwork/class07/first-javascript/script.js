5730b061a749c7e16339f001fc742f9e7d0e5128
frodriguez5008 <fdr2114@columnia.edu>
2017-05-14 22:20:14 -0400

    Week 4 Homework

---
+++ classwork/class07/first-javascript/script.js
@@ -0,0 +1,101 @@
+// console.log('MTA ISSUES');
+
+// // document.write('Where is my train?');
+
+// //console.log(10 * 10);
+
+// var answer1 = 10 * 10;
+// console.log(answer1);
+
+// //console.log(100 / 10);
+
+// // var answer2 = 100 / 10;
+// // console.log(answer2);
+
+// // //console.log(50 - 17);
+
+// // var answer3 = 50 - 17;
+// // console.log(answer3);
+
+// // //console.log(39 + 2)
+
+// // var answer4 = 39 + 2;
+// // console.log(answer4);
+
+// // //console.log('Frances ' + 'Rodriguez')
+
+// // var answer5 = 'Frances ' + 'Rodriguez';
+// // console.log(answer5);
+
+// var title = document.getElementById('title');
+// var info = document.querySelector('.info');
+
+// title.style.color = "tomato";
+
+// console.log(title);
+// console.log(info);
+
+
+
+// 1. create a new variable called codingFunLevel and set it equal to 5
+
+	var codingFunLevel = 5;
+
+// 2. use console.log() to print out the value of codingFunLevel
+
+	console.log(codingFunLevel);
+
+// 3. add 5 to the value of codingFunLevel (it should now equal 10)
+
+codingFunLevel = codingFunLevel + 5;
+
+// 4. use console.log() to print out the value of codingFunLevel again
+
+	console.log(codingFunLevel);
+
+// 5. create a variable called bestLanguage and set it equal to Javascript as a string.
+
+	var bestLanguage = 'Java' + 'script';
+
+// 6. console.log() the value of bestLanguage
+
+	console.log(bestLanguage);
+
+// // 7. Paste following element beneith the H1 in index.html:
+
+// //         <p id=“where”>Where in the world have you been?</p>
+
+// // 8. Create a variable called “whereElem” in script.js and set it equal to the element we just placed on the page.
+
+// //      hint: use a query method like document.querySelector
+
+// 	var whereElem = document.querySelector('#where');
+
+// 	console.log(whereElem);
+
+// // // 9. use the variable whereElem and set the background of the element it points to to rgb(0,255,0)
+
+// // //     hint: use style.background =
+
+// 	whereElem.style.background = 'rgb(0,255,0)';
+
+// // // 10. add “is the best langage ever!” to the variable bestLanguage
+
+// 	bestLanguage = bestlanguage + 'is the best language ever!';
+
+// // // 11. console.log() the value of bestLanguage
+
+// 	console.log(bestLanguage);
+
+
+	function whatDay() {
+		console.log('it is thursday');
+	}
+
+	whatDay()
+
+
+
+
+
+
