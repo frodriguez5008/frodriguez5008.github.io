d24c7de8eaba4f974dde441ef1d82c99405ebb12
frodriguez5008 <fdr2114@columnia.edu>
2017-05-28 21:12:05 -0400

    Week 6 Homework

--- homework/list-maker/js/app.js
+++ homework/list-maker/js/app.js
@@ -12,10 +12,28 @@
 
 $(document).ready(function () {
 
+	$('#clickme').click(function(){
+		var newItem = $('#item').val();
+		if (!newItem) {
+			alert ('List item is empty!')
+		} else 
+			appendItem(newItem);
+	});
+
+	function appendItem (newItem){
+		$('ul').append('<li>' + newItem + '</li>');
+		$('#item').val('');
+		$('#item').focus();
+		$('li').click(function() {
+			$(this).remove();
+		})
+	};
+});
+
 	// 1. Attach a .click() event to #clickme; attach an anonymous function to the .click()
 	// 1A. This anonymous function should create a variable, newItem equal to the value of #item
 	// 1B. This anonymous function should call a separate function, appendItem, and pass it the variable newItem
 	// 1C. This anonymous function should then clear the text inside of #item
 
 	// 2. Write the appendItem function here
-})
+
